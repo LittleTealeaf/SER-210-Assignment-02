218a6f90cb9aa2d24e3ad7c7f0e78940305e71ef
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-04 15:54:12 -0500

    Create CharacterListActivity.java

---
+++ app/src/main/java/ser/quinnipiac/edu/harrypottercharacters/app/CharacterListActivity.java
@@ -0,0 +1,16 @@
+package ser.quinnipiac.edu.harrypottercharacters.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ser.quinnipiac.edu.harrypottercharacters.R;
+
+public class CharacterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_list);
+    }
+}
